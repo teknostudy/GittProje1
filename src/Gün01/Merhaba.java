75d0c2ab9ed652209b59e641e1bc1358224676b9
teknostudy <erfatoserr@gmail.com>
2024-02-12 15:03:02 +0300

    Satın alma işlemi eklendi

--- src/Gün01/Merhaba.java
+++ src/Gün01/Merhaba.java
@@ -5,7 +5,13 @@ public class Merhaba {
 
         System.out.println("Merhaba 1");
         System.out.println("Merhaba 2");
-        System.out.println("Merhaba 3");
+
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("merhaba");
+        }
+
+
 
 
 
