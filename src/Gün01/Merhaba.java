2c5e50c4905d0a39bfd3c40c1c750c1908076acf
teknostudy <erfatoserr@gmail.com>
2024-02-12 14:53:44 +0300

    Sepette ürün eklenmesi ve kontrolü testi yapıldı

---
+++ src/Gün01/Merhaba.java
@@ -0,0 +1,12 @@
+package Gün01;
+
+public class Merhaba {
+    public static void main(String[] args) {
+
+        System.out.println("Merhaba 1");
+        System.out.println("Merhaba 2");
+        
+
+
+    }
+}
