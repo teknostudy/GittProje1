e918c37e79cfcd35bfa3bbab27823e592e5939e1
teknostudy <erfatoserr@gmail.com>
2024-02-12 15:05:47 +0300

    task2

--- src/Gün01/Merhaba.java
+++ src/Gün01/Merhaba.java
@@ -4,7 +4,9 @@ public class Merhaba {
     public static void main(String[] args) {
 
         System.out.println("Merhaba 1");
-        System.out.println("Merhaba 2");
+        for (int i = 0; i <5 ; i++) {
+            System.out.println("merhaba");
+        }
 
 
         for (int i = 0; i < 5; i++) {
